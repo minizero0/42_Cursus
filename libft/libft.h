041d375b24c9bada680fca10ea697e77df6c8f8c
yeongmin <dudals1069@gmail.com>
2021-11-18 17:27:14 +0900

    11-18

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 20:33:15 by yeocho            #+#    #+#             */
-/*   Updated: 2021/11/17 20:33:16 by yeocho           ###   ########.fr       */
+/*   Updated: 2021/11/18 17:26:16 by yeocho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@
 # include <stdlib.h>
 
 void	*ft_memset(void *ptr, int value, size_t num);
+void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *str);
-int	    ft_isalpha(int c);
-int	    ft_isalnum(int c);
-int	    ft_isascii(int c);
-int     ft_isdigit(int c);
-int	    ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 
 
 
--- libft/tese.c
+++ libft/tese.c
@@ -1,10 +1,10 @@
 #include "libft.h"
-
+#include <stdio.h>
 int	main(void)
 {
 	char *str;
 
 	str = "hello";
 	ft_bzero(str, 2);
-	printf(
+	printf("%s", str);
 }
