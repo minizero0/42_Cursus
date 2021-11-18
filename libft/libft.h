b368643ceee595c21104805c4e3a44895d907f6a
yeongmin <dudals1069@gmail.com>
2021-11-18 17:18:00 +0900

    11-18

--- libft/libft.h
+++ libft/libft.h
@@ -10,7 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef LIBFT_H
+#ifndef LIBFT_H
 # define LIBFT_H
+
+# include <stdlib.h>
+
+void	*ft_memset(void *ptr, int value, size_t num);
+int	    ft_isalpha(int c);
+int	    ft_isalnum(int c);
 
 #endif
---
+++ libft/tese.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+
+int	main(void)
+{
+	char *str;
+
+	str = "hello";
+	ft_bzero(str, 2);
+	printf(
+}
