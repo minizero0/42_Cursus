bf80f9447e805419894f8eb956843c8bfe08c949
yeongmin <dudals1069@gmail.com>
2022-01-04 15:44:10 +0900

    01-04

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -14,5 +14,24 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+    size_t  start;
+    size_t  end;
+    char    *ret;
 
+    if (!s1)
+        return (NULL);
+    if (!set)
+        return (ft_strdup(s1));
+    start = 0;
+    while (s1[start] && ft_strchr(set, s1[start]));
+        start++;
+    while (ft_strchr(set, s1[end]) && end >= start && s1[end])
+        end--;
+    if (start >= end)
+        return (ft_strdup(""));
+    ret = (char *)malloc(sizeof(char) * (end - start + 2));
+    if (!ret)
+        return (NULL);
+    ft_strlcpy(ret, s1 + start, end - start + 2);
+    return (ret);
 }
