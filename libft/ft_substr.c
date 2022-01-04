b4d67f0aacc78a3fbaa2a7bf2ca31d4c61a6915b
yeongmin <dudals1069@gmail.com>
2022-01-04 13:58:04 +0900

    01-04

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/22 14:59:38 by yeocho            #+#    #+#             */
-/*   Updated: 2021/11/22 15:16:57 by yeocho           ###   ########.fr       */
+/*   Created: 2022/01/04 13:34:29 by yeocho            #+#    #+#             */
+/*   Updated: 2022/01/04 13:35:04 by yeocho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	strlcpy(char *dst, const char *src, size_t dstsize)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	src_len;
-	size_t	i;
 
-	if (!dst)
-		return (0);
-	i = 0;
-	src_len = ft_strlen(src);
-	if (dstsize != 0)
-	{
-		while (src[i] && i < (dstsize - 1))
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	return (src_len);
 }
