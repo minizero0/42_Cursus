1389ffe78864ff0c52ec287ec5d6af991d27ddb7
yeongmin <dudals1069@gmail.com>
2022-01-04 14:55:04 +0900

    01-04

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/04 14:46:27 by yeocho            #+#    #+#             */
+/*   Updated: 2022/01/04 14:47:05 by yeocho           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+    char    *ret;
+    size_t  len1;
+    size_t  len2;
+
+    if (!s1 && !s2)
+        return (NULL);
+    if (!s1)
+        return (ft_strdup(s2));
+    if (!s2)
+        return (ft_strdup(s1));
+    len1 = ft_strlen(s1);
+    len2 = ft_strlen(s2);
+    ret = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+    if (!ret)
+        return (NULL);
+    ft_memcpy(ret, s1, len1);
+    ft_memcpy(ret + len1, s2, len2);
+    ret[len1 + len2] = '\0';
+    return (ret);
+
+
+}
