0b4ffc63e3b2588b04d9b194b468010a60fb8c6c
yeongmin <dudals1069@gmail.com>
2022-01-18 00:26:53 +0900

    01-18

--- libft/ft_putchar_fd.c
+++ libft/ft_putchar_fd.c
@@ -10,4 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <unistd.h>
 
+void    ft_putchar_fd(char c, int fd)
+{
+    if (fd < 0)
+        return ;
+    write(fd, &c, 1);
+}
