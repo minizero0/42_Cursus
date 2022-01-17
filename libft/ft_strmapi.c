6619d4f440b3f08a1e1b9255a7759fc6d3c22d3d
yeongmin <dudals1069@gmail.com>
2022-01-17 23:38:05 +0900

    01-17

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -10,4 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "libft.h"
 
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char            *ret;
+    unsigned char   length;
+    unsigned char   index;
+
+    if (!s || !f)
+        return (0);
+    length = ft_strlen(s);
+    index = 0;
+    ret = (char *)malloc(sizeof(char) * (length + 1));
+    if (!ret)
+        return (NULL);
+    while (index < length)
+    {
+        ret[index] = f(index, s[index]);
+        index++;
+    }
+    ret[index] = '\0';
+    return (ret);
+}
