e684c7ebc6047d8a2a21034de9563622fae711a4
yeongmin <dudals1069@gmail.com>
2022-01-19 16:18:02 +0900

    01-19

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 16:16:54 by yeocho            #+#    #+#             */
+/*   Updated: 2022/01/19 16:17:55 by yeocho           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	if (*lst == NULL)
+		*lst = new;
+	else
+		(ft_lstlast(*lst))->next = new;
+}
