b87f68f728f0a036e4d8308bf0ac1e00e1445288
yeongmin <dudals1069@gmail.com>
2022-01-19 16:38:38 +0900

    01-19

---
+++ libft/ft_lstdelone.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 16:35:17 by yeocho            #+#    #+#             */
+/*   Updated: 2022/01/19 16:38:30 by yeocho           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst == NULL || del == NULL)
+		return ;
+	(*del)(lst->content);
+	free(lst);
+}
