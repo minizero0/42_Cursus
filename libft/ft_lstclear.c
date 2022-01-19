34cdf34b881254c44137a1c26a13455a7eeb6d80
yeongmin <dudals1069@gmail.com>
2022-01-19 16:41:27 +0900

    01-19

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 16:39:17 by yeocho            #+#    #+#             */
+/*   Updated: 2022/01/19 16:41:20 by yeocho           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*curr;
+	t_list	*next;
+
+	if (!lst || !del)
+		return ;
+	curr = *lst;
+	while (curr)
+	{
+		next = curr->next;
+		ft_lstdelone(curr, del);
+		curr = next;
+	}
+	*lst = NULL;
+}
