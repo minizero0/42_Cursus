5e315e202a72457b8c43d75787982b04e16f3898
yeongmin <dudals1069@gmail.com>
2022-01-21 11:04:02 +0900

    01-21

--- libft/ft_tolower.c
+++ libft/ft_tolower.c
@@ -6,13 +6,13 @@
 /*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 15:37:07 by yeocho            #+#    #+#             */
-/*   Updated: 2021/11/22 15:38:17 by yeocho           ###   ########.fr       */
+/*   Updated: 2022/01/21 11:03:51 by yeocho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_lower(int c)
+int	ft_tolower(int c)
 {
 	if (c >= 65 && c <= 90)
 		c += 32;
