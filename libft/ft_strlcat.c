1127033101bac89708e5fe70aa72b249356697a9
yeongmin <dudals1069@gmail.com>
2022-01-21 11:02:19 +0900

    01-21

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,33 +6,33 @@
 /*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 15:17:32 by yeocho            #+#    #+#             */
-/*   Updated: 2021/11/22 15:30:42 by yeocho           ###   ########.fr       */
+/*   Updated: 2022/01/21 11:02:04 by yeocho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
-size_t	strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	len1;
-	size_t	len2;
 	size_t	index;
+	size_t	dst_len;
+	size_t	src_len;
 
 	index = 0;
-	len1 = ft_strlen(dst);
-	len2 = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
 	if (!dstsize)
-		return (len2);
-	if (dstsize <= len1)
-		return (len1 + len2);
+		return (src_len);
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
 	while (*dst)
 		dst++;
-	while (src[index] && index < (dstsize - len1 - 1))
+	while (src[index] && index < (dstsize - dst_len - 1))
 	{
 		*dst = src[index];
 		index++;
 		dst++;
 	}
 	*dst = '\0';
-	return (len1 + len2);
+	return (dst_len + src_len);
 }
