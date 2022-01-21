713fec6ec5f449019881abb4a02e6cbb0d3a2b8a
yeongmin <dudals1069@gmail.com>
2022-01-21 11:48:45 +0900

    01-21

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,33 +6,32 @@
 /*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 14:46:27 by yeocho            #+#    #+#             */
-/*   Updated: 2022/01/04 14:47:05 by yeocho           ###   ########.fr       */
+/*   Updated: 2022/01/21 11:48:38 by yeocho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-    char    *ret;
-    size_t  len1;
-    size_t  len2;
+	char	*ret;
+	size_t	len1;
+	size_t	len2;
 
-    if (!s1 && !s2)
-        return (NULL);
-    if (!s1)
-        return (ft_strdup(s2));
-    if (!s2)
-        return (ft_strdup(s1));
-    len1 = ft_strlen(s1);
-    len2 = ft_strlen(s2);
-    ret = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-    if (!ret)
-        return (NULL);
-    ft_memcpy(ret, s1, len1);
-    ft_memcpy(ret + len1, s2, len2);
-    ret[len1 + len2] = '\0';
-    return (ret);
-
-
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	ret = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!ret)
+		return (NULL);
+	ft_memcpy(ret, s1, len1);
+	ft_memcpy(ret + len1, s2, len2);
+	ret[len1 + len2] = '\0';
+	return (ret);
 }
