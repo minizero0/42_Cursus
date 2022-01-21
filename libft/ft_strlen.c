e4d5132af4d81ed8525ea407c22c4ad61055764b
yeongmin <dudals1069@gmail.com>
2022-01-21 10:47:22 +0900

    01-21

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,18 +6,18 @@
 /*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 20:51:04 by yeocho            #+#    #+#             */
-/*   Updated: 2021/11/17 21:01:45 by yeocho           ###   ########.fr       */
+/*   Updated: 2022/01/21 10:46:51 by yeocho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlen(const char *s)
 {
 	size_t	index;
 
 	index = 0;
-	while (str[index])
+	while (s[index])
 		index++;
 	return (index);
 }
