582f8b5f2268866292b9c532839321fdd6f57b12
yeongmin <dudals1069@gmail.com>
2022-01-21 12:37:21 +0900

    01-21

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,94 +6,96 @@
 /*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 16:12:57 by yeocho            #+#    #+#             */
-/*   Updated: 2022/01/04 16:13:27 by yeocho           ###   ########.fr       */
+/*   Updated: 2022/01/21 12:13:14 by yeocho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
-int count_word(char const *s, char c)
+int	count_word(char const *s, char c)
 {
-    int     count;
-    int     i;
+	int		count;
+	int		i;
 
-    i = 0;
-    count  = 0;
-    if (*s == '\0')
-        return (0);
-    while (s[i] && s[i] == c)
-        i++;
-    while (s[i])
-    {
-        if (s[i] == c)
-        {
-            count++;
-            while (s[i] && s[i] == c)
-                i++;
-            continue;
-        }
-        i++;
-    }
-    if (s[i - 1] != c)
-        count++;
-    return (count);
+	i = 0;
+	count = 0;
+	if (*s == '\0')
+		return (0);
+	while (s[i] && s[i] == c)
+		i++;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			count++;
+			while (s[i] && s[i] == c)
+				i++;
+			continue ;
+		}
+		i++;
+	}
+	if (s[i - 1] != c)
+		count++;
+	return (count);
 }
 
-static int  count_ch(char const *s, char c)
+static int	count_ch(char const *s, char c)
 {
-    int     count;
-    int     i;
+	int	count;
+	int	i;
 
-    i = 0;
-    count = 0;
-    while (s[i] != c && s[i])
-    {
-        count++;
-        i++;
-    }
-    return (count);
+	i = 0;
+	count = 0;
+	while (s[i] != c && s[i])
+	{
+		count++;
+		i++;
+	}
+	return (count);
 }
 
-static char **malloc_error(char **ret, int i)
+static char	**malloc_error(char **ret, int i)
 {
-    int     j;
-    if (!ret)
-    {
-        j = 0;
-        while (j < i)
-            free(ret[j++]);
-        free(ret);
-        return (NULL);
-    }
-    else
-        return (NULL);
+	int		j;
+
+	if (!ret)
+	{
+		j = 0;
+		while (j < i)
+			free(ret[j++]);
+		free(ret);
+		return (NULL);
+	}
+	else
+		return (NULL);
 }
 
 char	**ft_split(char const *s, char c)
 {
-    char    **ret;
-    int     i;
-    int     j;
+	char	**ret;
+	int		i;
+	int		j;
 
-    if (!s)
-        return (NULL);
-    ret = (char **)malloc(sizeof(char *) * (count_word(s, c) + 1));
-    if (!ret)
-        return (NULL);
-    i = 0;
-    while (*s)
-    {
-        j = 0;
-        while (*s && *s == c)
-            s++;
-        if (!count_ch(s, c))
-            break;
-        ret[i] = (char *)malloc(sizeof(char) * (count_ch(s, c) + 1));
+	if (!s)
+		return (NULL);
+	ret = (char **)malloc(sizeof(char *) * (count_word(s, c) + 1));
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		j = 0;
+		while (*s && *s == c)
+			s++;
+		if (!count_ch(s, c))
+			break ;
+		ret[i] = (char *)malloc(sizeof(char) * (count_ch(s, c) + 1));
 		malloc_error(ret, i);
 		while (*s && *s != c)
 			ret[i][j++] = *s++;
 		ret[i++][j] = '\0';
-    } 
-    ret[i] = NULL;
+	}
+	ret[i] = NULL;
 	return (ret);
 }
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,32 +6,34 @@
 /*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 15:16:06 by yeocho            #+#    #+#             */
-/*   Updated: 2022/01/04 15:16:43 by yeocho           ###   ########.fr       */
+/*   Updated: 2022/01/21 11:59:08 by yeocho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-    size_t  start;
-    size_t  end;
-    char    *ret;
+	size_t	start;
+	size_t	end;
+	char	*ret;
 
-    if (!s1)
-        return (NULL);
-    if (!set)
-        return (ft_strdup(s1));
-    start = 0;
-    while (s1[start] && ft_strchr(set, s1[start]))
-        start++;
-    while (ft_strchr(set, s1[end]) && end >= start && s1[end])
-        end--;
-    if (end >= start)
-        return (ft_strdup(""));
-    ret = (char *)malloc(sizeof(char) * (end - start + 2));
-    if (!ret)
-        return (NULL);
-    ft_strlcpy(ret, s1 + start, end - start + 2);
-    return (ret);
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (ft_strchr(set, s1[end]) && end >= start && s1[end])
+		end--;
+	if (start >= end)
+		return (ft_strdup(""));
+	ret = (char *)malloc(sizeof(char) * (end - start + 2));
+	if (!ret)
+		return (NULL);
+	ft_strlcpy(ret, s1 + start, end - start + 2);
+	return (ret);
 }
