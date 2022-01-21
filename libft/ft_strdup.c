5ac1bbefe965fc26fdd8123317356a9ab459b142
yeongmin <dudals1069@gmail.com>
2022-01-21 11:39:36 +0900

    01-21

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,26 +6,26 @@
 /*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 12:52:32 by yeocho            #+#    #+#             */
-/*   Updated: 2022/01/04 12:53:24 by yeocho           ###   ########.fr       */
+/*   Updated: 2022/01/21 11:39:29 by yeocho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
-    char    *ret;
-    int     index;
+	char	*ret;
+	int		index;
 
-    index = 0;
-    ret = (char *)malloc(sizeof(char) * (ft_strlen(s1) * + 1));
-    if (!ret)
-        return (NULL);
-    while (s1[index])
-    {
-        ret[index] = s1[index];
-        index++;
-    }
-    ret[index] = '\0';
-    return (ret);
+	index = 0;
+	ret = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (!ret)
+		return (NULL);
+	while (s1[index])
+	{
+		ret[index] = s1[index];
+		index++;
+	}
+	ret[index] = '\0';
+	return (ret);
 }
