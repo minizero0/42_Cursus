d85e4b57ee83a5aab0b056e26788154bbd431455
yeongmin <dudals1069@gmail.com>
2022-01-22 16:59:00 +0900

    01-22

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,31 +6,31 @@
 /*   By: yeocho <yeocho@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 15:57:18 by yeocho            #+#    #+#             */
-/*   Updated: 2022/01/17 23:09:33 by yeocho           ###   ########.fr       */
+/*   Updated: 2022/01/22 16:58:51 by yeocho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    char            *ret;
-    unsigned char   length;
-    unsigned char   index;
+	char			*ret;
+	unsigned char	length;
+	unsigned char	index;
 
-    if (!s || !f)
-        return (0);
-    length = ft_strlen(s);
-    index = 0;
-    ret = (char *)malloc(sizeof(char) * (length + 1));
-    if (!ret)
-        return (NULL);
-    while (index < length)
-    {
-        ret[index] = f(index, s[index]);
-        index++;
-    }
-    ret[index] = '\0';
-    return (ret);
+	if (!s || !f)
+		return (0);
+	length = ft_strlen(s);
+	index = 0;
+	ret = (char *)malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	while (index < length)
+	{
+		ret[index] = f(index, s[index]);
+		index++;
+	}
+	ret[index] = '\0';
+	return (ret);
 }
